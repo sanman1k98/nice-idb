5e2470124a2035970c8159fc6a1dc3a3f0f353c5
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-09-18 22:21:32 -0400

    chore: customize ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,13 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-
+	typescript: true,
+	stylistic: {
+		indent: 'tab',
+		semi: true,
+		quotes: 'single',
+		overrides: {
+			'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+		},
+	},
 })
