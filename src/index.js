602ecb6e5f08eac8ee33aa672dc31391f05fc270
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2026-01-03 10:00:32 -0500

    feat(types): re-export types from main entry

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+/**
+ * @typedef {import('./db.js').NiceIDBUpgradeCallback} NiceIDBUpgradeCallback
+ * @typedef {import('./util.js').NiceIDBError} NiceIDBError
+ * @typedef {import('./util.js').NiceIDBErrorInfo} NiceIDBErrorInfo
+ * @typedef {import('./util.js').KeyRangeOptions} KeyRangeOptions
+ * @typedef {import('./util.js').CursorOptions} CursorOptions
+ */
+
 export { NiceIDB } from './db.js';
