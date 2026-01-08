01027a0ba9d3cb10100e92aeb8fe45a50c07d248
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2026-01-08 13:53:03 -0500

    feat: export transaction, store, and index types

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@
  * @typedef {import('./db.js').DefineDatabaseSchema} DefineDatabaseSchema
  * @typedef {import('./db.js').DefineVersionedUpgrade} DefineVersionedUpgrade
  * @typedef {import('./db.js').UpgradeCallback} UpgradeCallback
+ *
+ * @typedef {import('./tx.js').NiceIDBTransaction} NiceIDBTransaction
+ *
+ * @typedef {import('./store.js').NiceIDBObjectStore} NiceIDBObjectStore
+ *
+ * @typedef {import('./idx.js').NiceIDBIndex} NiceIDBIndex
+ *
  * @typedef {import('./util.js').NiceIDBError} NiceIDBError
  * @typedef {import('./util.js').NiceIDBErrorInfo} NiceIDBErrorInfo
  * @typedef {import('./util.js').KeyRangeOptions} KeyRangeOptions
