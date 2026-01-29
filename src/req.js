8cb89f857b71aced96a1a701a07211bd9eff71e7
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2026-01-29 11:50:47 -0500

    docs: more examples and descriptions

--- src/req.js
+++ src/req.js
@@ -61,15 +61,38 @@ export class NiceIDBEventTarget {
 }
 
 /**
+ * A "thenable" wrapper for `IDBRequest` objects. Use await to get the
+ * underlying request's `result` property.
+ *
  * @example
- * // Wrap some existing IDBRequest and await it to get its result.
  * const req = new NiceIDBRequest(someIDBRequest);
  * const result = await req;
+ * console.assert(result === someIDBRequest.result);
+ *
+ * @example
+ * // Transform the fulfillment value with a callback as a second constructor argument.
+ * const req = new NiceIDBRequest(indexedDB.open('my-db'), (result) => {
+ *   console.assert(result instanceof IDBDatabase);
+ *   console.info(`Sucessfully opened database ${result.name}`);
+ *   // Return a NiceIDB instance as the fulfillment value.
+ *   return new NiceIDB(result);
+ * });
+ *
+ * // Register some event listeners...
+ * req
+ *   .once('blocked', (event) => { ... })
+ *   .once('error', (event) => { ... });
+ *
+ * // Await the request to get the transformed result.
+ * const db = await req;
+ * console.assert(!(db instanceof IDBDatabase));
+ * console.assert(db instanceof NiceIDB);
  *
  * @example
  * // Attach some event listeners.
- * const req = new NiceIDBRequest(someIDBRequest);
- * req.on('error', (event) => event.preventDefault());
+ * const req = new NiceIDBRequest(someIDBRequest).once('error', (event) => {
+ *   event.preventDefault()
+ * });
  * const result = await req;
  *
  * @template {IDBRequest} R
@@ -84,8 +107,8 @@ export class NiceIDBRequest extends NiceIDBEventTarget {
 	/** @type {Promise<TResolved>} */ #promise;
 
 	/**
-	 * @param {R} request
-	 * @param {(value: R['result']) => TResolved | PromiseLike<TResolved>} [onfulfilled]
+	 * @param {R} request - the IDBRequest to wrap.
+	 * @param {(value: R['result']) => TResolved | PromiseLike<TResolved>} [onfulfilled] - Optionally transform the `result` that the request will resolve to.
 	 * @param {(reason: any) => PromiseLike<TRejected>} [onrejected]
 	 */
 	constructor(request, onfulfilled, onrejected) {
