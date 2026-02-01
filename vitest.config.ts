b8404e778123de1b225165ebb6d9f433cf6dfdcc
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2026-01-31 23:31:16 -0500

    build(test): add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { webdriverio } from '@vitest/browser-webdriverio';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		browser: {
+			enabled: true,
+			provider: webdriverio(),
+			// https://vitest.dev/config/browser/webdriverio
+			instances: [
+				{ browser: 'safari' },
+			],
+		},
+	},
+});
